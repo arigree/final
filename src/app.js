d7130586f1ac8f3ca19790df1dbf07c389275607
arigree <arigree@iu.edu>
2024-12-09 10:39:13 -0500

    Adding login sign up log out, styling, responsiveness

--- src/app.js
+++ src/app.js
@@ -1,24 +1,57 @@
 import * as $ from "jquery";
 import { initURLListener } from "./model";
+import { signUserUp, signUserOut, signUserIn } from "./model";
 
+function initListeners() {
+  console.log("Initializing listeners...");
+
+  if ($("#submit")) {
+    $(document).on("click", "#submit", (e) => {
+      e.preventDefault();
+      console.log("Sign-up button clicked");
+      const firstName = $("#fName").val();
+      const lastName = $("#lName").val();
+      const email = $("#email").val();
+      const password = $("#password").val();
+      signUserUp(firstName, lastName, email, password);
+    });
+  }
+
+  if ($("#siSubmit")) {
+    $(document).on("click", "#siSubmit", (e) => {
+      e.preventDefault();
+      console.log("Login button clicked");
+      const siEmail = $("#siEmail").val();
+      const siPassword = $("#siPassword").val();
+      signUserIn(siEmail, siPassword);
+    });
+  }
+
+  $(document).on("click", "#so", () => {
+    signUserOut();
+  });
+}
 function initAccountListener() {
-    // console.log("hello");
-    $(".logo").on("click", function () {
-        window.location.hash="home";
-        console.log("logo clicked");
-      });
-  $(".account").on("click", function () {
-    window.location.hash="account";
-    console.log("account clicked");
-  });
-  $(".cart").on("click", function () {
-    window.location.hash="cart";
-    console.log("cart clicked");
-    
-  });
+  // console.log("hello");
+  $(".logo").on("click", function () {
+      window.location.hash="home";
+      console.log("logo clicked");
+    });
+$(".account").on("click", function () {
+  window.location.hash="account";
+  console.log("account clicked");
+});
+$(".cart").on("click", function () {
+  window.location.hash="cart";
+  console.log("cart clicked");
+  
+});
 };
 
+
+
 $(document).ready(function () {
+  initListeners();
   initURLListener();
   initAccountListener();
 });
--- src/model.js
+++ src/model.js
@@ -1,4 +1,55 @@
 import * as $ from "jquery";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { app } from "./firebaseConfig";
+
+export const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User is signed in");
+    
+    }
+  }
+);
+
+export function signUserUp(fn, ln, email, password) {
+  // console.log("model.js" + fn, ln, email, password);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      console.log("User Created");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function signUserOut() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out");
+      })
+      .catch((error) => {
+        console.log("Error signing out:", error);
+      });
+  }
+
+export function signUserIn(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.hash = "#yourRecipes";
+        console.log("User Signed in");
+      })
+      .catch((error) => {
+        console.log("Error during sign-in:", error.message);
+      });
+  }
 
 var products = [];
 var cart = [];
@@ -18,6 +69,9 @@ function changeRoute() {
       $.get(`pages/${pageID}.html`, function (data) {
         $("#app").html(data);
         if (pageID === "cart") {
+          // if (products.length === 0){
+          //   loadProducts();
+          // }
           loadCartItems();
         }
       });
@@ -28,44 +82,15 @@ function changeRoute() {
 }
 
 
-function loadCartItems(){
-    if(cart.length > 0){
-        $(".cart-items").html("");
-        $.each(cart, (index, productIndex)=>{
-            let product = products[productIndex];
-            console.log("cart page", cart);
-            let cartHTML = `<div class="product">
-            <div class="image-holder">
-      <img src="${product.productImage}" />
-    </div>
-    <div class="productName">${product.productName}</div>
-            <div class="description">
-            ${product.productDescription}
-            </div>
-            <div class="price">${product.productPrice}</div>
-            <div class="remove" id="${index}">Remove</div>
-            </div>`;
-
-            $(".cart-items").append(cartHTML);
-
-            $(".remove").on("click", function (){
-                console.log('remove', cart);
-                cart.splice(index, 1);
-                console.log("remove after", cart);
-                loadCartItems();
-            })
-        });
-    }
-};
-
-function loadHomePage() {
-  $("#app").html("<header><img src='images/header.webp'/><div class='buttons'></div></header><div class='products'></div>");
-  $.each(products, (index, product) => {
-    let productHTML = `
-    
-    <div class="product">
+function loadCartItems() {
+  if (cart.length > 0) {
+      $(".cart-items").html("");
+      $.each(cart, (index, productIndex) => {
+          let product = products[Number(productIndex)];
+          if (product) {
+              let cartHTML = `<div class="product">
     ${
-        product.productBanner ? `<div class="pbanner" style="background-color: ${product.productBannerColor}">${product.productBanner}</div>` 
+        product.productBanner ? `<div class="pbanner" style="background-color: ${product.productBannerColor}; color: ${product.productFontColor}">${product.productBanner}</div>` 
         : ""
     }
         <div class="image-holder">
@@ -74,7 +99,52 @@ function loadHomePage() {
             <div class="productName">${product.productName}</div>
       <div class="priceHolder">
       <div class="price">
-          ${product.productPrice}
+      <span>$</span>${product.productPrice}
+        </div>
+        <p>with Keurig® Starter Kit</p>
+      </div>
+        
+        <div class="oldPrice">${product.oldPrice}</div>
+        <div class="rating">
+        <div class="stars">
+        <img src="${product.ratingImg}"/>
+        </div>
+        <div class="ratingTxt">${product.rating}</div>
+        </div>
+        <div class="remove" id="${index}">Remove</div>
+      </div>`;
+              $(".cart-items").append(cartHTML);
+          }
+      });
+
+      $(".remove").on("click", function () {
+          let removeIndex = parseInt($(this).attr("id"), 10);
+          cart.splice(removeIndex, 1);
+          loadCartItems();
+      });
+  } else {
+      $(".cart-items").html("<p>Your cart is empty.</p>");
+  }
+}
+
+
+function loadHomePage() {
+  $("#app").html("<header><img src='images/header.webp'/><div class='sortBtns'><div class='buttons'><div class='button'>Categories <i class='fa-solid fa-angle-down'></i></div><div class='button'>Features <i class='fa-solid fa-angle-down'></i></div><div class='button'>Brew Type <i class='fa-solid fa-angle-down'></i></div><div class='button'>Reservoir <i class='fa-solid fa-angle-down'></i></div><div class='button'>Commercial <i class='fa-solid fa-angle-down'></i></div><div class='button'>Color <i class='fa-solid fa-angle-down'></i></div></div><div class='sort'><span>Sort by</span><p>Popularity (all time) <i class='fa-solid fa-angle-down'></i></p></div></div></header><div class='products'></div>");
+  $.each(products, (index, product) => {
+    let productHTML = `
+    
+    <div class="product">
+    ${
+        product.productBanner ? `<div class="pbanner" style="background-color: ${product.productBannerColor}; color: ${product.productFontColor}">${product.productBanner}</div>` 
+        : ""
+    }
+        <div class="image-holder">
+          <img src="${product.productImage}" />
+        </div>
+            <div class="productName">${product.productName}</div>
+      <div class="priceHolder">
+      <div class="price">
+      <span>$</span>${product.productPrice}
         </div>
         <p>with Keurig® Starter Kit</p>
       </div>
@@ -101,10 +171,11 @@ function loadHomePage() {
 
 function addBuyNowListener(){
     $(".buy-now").on("click", function (){
-        let index = $(this).attr("id");
+      let index = parseInt($(this).attr("id"), 10);
         cart.push(index);
         $(".item-text").html(cart.length);
         console.log("click buy now", index);
+        alert("Item added to cart.")
     })
 };
 
